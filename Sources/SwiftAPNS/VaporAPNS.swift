a269818e8d902b49688c23e6b4b681db32b3799a
matthijs2704 <matthijs.logemann@xs4all.nl>
2016-09-23 16:12:06 +0200

    Added Options to the initializer, connect to the apple push services server on init

--- Sources/SwiftAPNS/VaporAPNS.swift
+++ Sources/SwiftAPNS/VaporAPNS.swift
@@ -8,31 +8,39 @@ public class VaporAPNS {
     private var options: Options!
     private var apnsAuthKey: String!
     
-    public init?(authKeyPath: String) {
+    public init?(authKeyPath: String, options: Options? = nil) throws {
         guard let tokenStr = tokenStringFor(authKeyPath: authKeyPath) else {
             print ("VaporAPNS -- AuthKey file invalid")
             return nil
         }
         self.apnsAuthKey = tokenStr
+        
+        self.options = options ?? Options()
+        
+        try connect()
     }
     
+    private func connect() throws {
+        httpClient = try Client<TCPClientStream, Serializer<Request>, Parser<Response>>.init(scheme: "https", host: self.hostURL(development: self.options.development), port: self.options.port.rawValue)
+    }
     
-    
-    
+    public func send(applePushMessage message: ApplePushMessage) throws {
+        try httpClient.post(path: "/3/device/")
+    }
     
 }
 
 extension VaporAPNS {
     
-    private func baseURL(development: Bool, port: Options.Port) -> NSURL {
+    fileprivate func hostURL(development: Bool) -> String {
         if development {
-            return NSURL(string: "https://api.development.push.apple.com:\(port)/3/device/")!
+            return "api.development.push.apple.com" //   "
         } else {
-            return NSURL(string: "https://api.push.apple.com:\(port)/3/device/")!
+            return "api.push.apple.com" //   /3/device/"
         }
     }
     
-    func tokenStringFor(authKeyPath: String) -> String? {
+    fileprivate func tokenStringFor(authKeyPath: String) -> String? {
         guard let fileData = NSData(contentsOfFile: authKeyPath) as? Data else {
             return nil
         }
