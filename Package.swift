b5fe4f43c79830ccf6dbabd30f59373045756b26
matthijs2704 <matthijs.logemann@xs4all.nl>
2016-09-26 08:31:00 +0200

    Added CCurl library

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ let package = Package(
     name: "VaporAPNS",
     dependencies: [
         .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 1, minor: 0)
+        .Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/boostcode/CCurl.git", majorVersion: 0, minor: 2)
     ]
 )
