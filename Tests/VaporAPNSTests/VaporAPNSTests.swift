19430c1cdbca56ddcbb2f5e4fe489c1c16869e10
matthijs2704 <matthijs.logemann@xs4all.nl>
2016-10-02 16:59:27 +0200

    Disable unrunnable test (will come back later)

--- Tests/VaporAPNSTests/VaporAPNSTests.swift
+++ Tests/VaporAPNSTests/VaporAPNSTests.swift
@@ -21,13 +21,13 @@ class VaporAPNSTests: XCTestCase { // TODO: Set this up so others can test this 
     }
     
     func testSendPush() throws {
-        let correctVaporAPNSInstance = try VaporAPNS(certPath: "/Users/matthijs/Downloads/newfile.crt.pem", keyPath: "/Users/matthijs/Downloads/newfile.key.pem")
-        
-        let pl = Payload(title: "Hello", body: "from here! :D")
-        let pushMessage = ApplePushMessage(topic: "nl.logicbit.ReviusSchoolkrant", priority: .immediately, payload: pl, deviceToken: "488681b8e30e6722012aeb88f485c823b9be15c42e6cc8db1550a8f1abb590d7", sandbox: true)
-        
-        let t = correctVaporAPNSInstance.send(applePushMessage: pushMessage)
-//        let t2 = correctVaporAPNSInstance.send(applePushMessage: pushMessage)
-        print (t)
+//        let correctVaporAPNSInstance = try VaporAPNS(certPath: "/Users/matthijs/Downloads/newfile.crt.pem", keyPath: "/Users/matthijs/Downloads/newfile.key.pem")
+//        
+//        let pl = Payload(title: "Hello", body: "from here! :D")
+//        let pushMessage = ApplePushMessage(topic: "nl.logicbit.ReviusSchoolkrant", priority: .immediately, payload: pl, deviceToken: "488681b8e30e6722012aeb88f485c823b9be15c42e6cc8db1550a8f1abb590d7", sandbox: true)
+//        
+//        let t = correctVaporAPNSInstance.send(applePushMessage: pushMessage)
+////        let t2 = correctVaporAPNSInstance.send(applePushMessage: pushMessage)
+//        print (t)
     }
 }
