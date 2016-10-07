5697534840d51bee9889af41cdda2cf4b8855f12
matthijs2704 <matthijs.logemann@xs4all.nl>
2016-10-07 23:41:35 +0200

    Made all payload properties public

--- Sources/VaporAPNS/Payload.swift
+++ Sources/VaporAPNS/Payload.swift
@@ -11,43 +11,46 @@ import JSON
 
 open class Payload: JSONRepresentable {
     /// The number to display as the badge of the app icon.
-    var badge: Int?
+    public var badge: Int?
     
     /// A short string describing the purpose of the notification. Apple Watch displays this string as part of the notification interface. This string is displayed only briefly and should be crafted so that it can be understood quickly. This key was added in iOS 8.2.
-    var title: String?
+    public var title: String?
     
     /// The text of the alert message. Can be nil if using titleLocKey
-    var body: String?
+    public var body: String?
     
     /// The key to a title string in the Localizable.strings file for the current localization. The key string can be formatted with %@ and %n$@ specifiers to take the variables specified in the titleLocArgs array.
-    var titleLocKey: String?
+    public var titleLocKey: String?
     
     /// Variable string values to appear in place of the format specifiers in titleLocKey.
-    var titleLocArgs: [String]?
+    public var titleLocArgs: [String]?
     
     /// If a string is specified, the system displays an alert that includes the Close and View buttons. The string is used as a key to get a localized string in the current localization to use for the right button’s title instead of “View”.
-    var actionLocKey: String?
+    public var actionLocKey: String?
     
     /// A key to an alert-message string in a Localizable.strings file for the current localization (which is set by the user’s language preference). The key string can be formatted with %@ and %n$@ specifiers to take the variables specified in the bodyLocArgs array.
-    var bodyLocKey: String?
+    public var bodyLocKey: String?
     
     /// Variable string values to appear in place of the format specifiers in bodyLocKey.
-    var bodyLocArgs: [String]?
+    public var bodyLocArgs: [String]?
     
     /// The filename of an image file in the app bundle, with or without the filename extension. The image is used as the launch image when users tap the action button or move the action slider. If this property is not specified, the system either uses the previous snapshot, uses the image identified by the UILaunchImageFile key in the app’s Info.plist file, or falls back to Default.png.
-    var launchImage: String?
+    public var launchImage: String?
     
     /// The name of a sound file in the app bundle or in the Library/Sounds folder of the app’s data container. The sound in this file is played as an alert. If the sound file doesn’t exist or default is specified as the value, the default alert sound is played. 
-    var sound: String?
+    public var sound: String?
     
     /// Silent push notification. This automatically ignores any other push message keys (title, body, ect.) and only the extra key-value pairs are added to the final payload
-    var contentAvailable: Bool = false
+    public var contentAvailable: Bool = false
     
     /// When displaying notifications, the system visually groups notifications with the same thread identifier together.
-    var threadId: String?
+    public var threadId: String?
 
     // Any extra key-value pairs to add to the JSON
-    var extra: [String: NodeRepresentable] = [:]
+    public var extra: [String: NodeRepresentable] = [:]
+    
+    // Simple, empty initializer
+    public init() {}
     
     open func makeJSON() throws -> JSON {
         var payloadData: [String: NodeRepresentable] = [:]
